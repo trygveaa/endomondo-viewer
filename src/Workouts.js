2798b626957a00f3f356963489e2eff969945977
Trygve Aaberge <trygveaa@gmail.com>
2021-01-09 19:10:53 +0100

    Add a modal for showing larger pictures

--- src/Workouts.js
+++ src/Workouts.js
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import Modal from "react-modal";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import nbLocale from "@fullcalendar/core/locales/nb";
 import Map from "./Map";
 import sports from "./public/sports";
+
+Modal.setAppElement("#root");
 
 const userid = new URLSearchParams(window.location.search).get("userid");
 
@@ -21,6 +24,8 @@ function historyToEvents(history) {
 export default function Workouts() {
   const [events, setEvents] = useState([]);
   const [currentEvent, setCurrentEvent] = useState(null);
+  const [currentPicture, setCurrentPicture] = useState(null);
+  const modalContent = useRef(null);
 
   async function fetchHistory(dates) {
     const months = [];
@@ -58,6 +63,29 @@ export default function Workouts() {
     setCurrentEvent({ details });
   }
 
+  function changePictureRelative(relativeIndex) {
+    const pictures = currentEvent.details.pictures;
+    const currentIndex = pictures.findIndex(
+      (picture) => picture.id === currentPicture.id
+    );
+    const newIndex = currentIndex + relativeIndex;
+    if (newIndex < 0) {
+      setCurrentPicture(pictures[pictures.length - 1]);
+    } else if (newIndex >= pictures.length) {
+      setCurrentPicture(pictures[0]);
+    } else {
+      setCurrentPicture(pictures[newIndex]);
+    }
+  }
+
+  function onKeyDownModal(event) {
+    if (event.key === "ArrowLeft") {
+      changePictureRelative(-1);
+    } else if (event.key === "ArrowRight") {
+      changePictureRelative(1);
+    }
+  }
+
   return (
     <div style={{ maxWidth: "1000px", margin: "0 auto" }}>
       <div style={{ margin: "0 16px" }}>
@@ -91,10 +119,12 @@ export default function Workouts() {
                   style={{
                     width: "calc(33.333333% - 16px)",
                     margin: "8px",
+                    objectFit: "contain",
                   }}
                   key={picture.id}
                   src={picture.url}
                   alt=""
+                  onClick={() => setCurrentPicture(picture)}
                 />
               ))}
             </div>
@@ -102,6 +132,51 @@ export default function Workouts() {
             <div style={{ margin: "16px 0" }}>
               <Map activity={currentEvent.details} />
             </div>
+
+            <Modal
+              isOpen={currentPicture != null}
+              style={{ overlay: { zIndex: 2 } }}
+              onRequestClose={() => setCurrentPicture(null)}
+              onAfterOpen={() => modalContent.current.focus()}
+            >
+              <div
+                ref={modalContent}
+                tabIndex="-1"
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  height: "100%",
+                  outline: "none",
+                }}
+                onKeyDown={onKeyDownModal}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    marginBottom: "16px",
+                  }}
+                >
+                  <button onClick={() => changePictureRelative(-1)}>
+                    Forrige
+                  </button>
+                  <button onClick={() => setCurrentPicture(null)}>Lukk</button>
+                  <button onClick={() => changePictureRelative(1)}>
+                    Neste
+                  </button>
+                </div>
+                <img
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    minHeight: 0,
+                    objectFit: "contain",
+                  }}
+                  src={currentPicture?.url}
+                  alt=""
+                />
+              </div>
+            </Modal>
           </>
         )}
       </div>
